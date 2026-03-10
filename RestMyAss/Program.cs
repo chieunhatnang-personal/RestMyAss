d880407a7089c9c2b60fa77b9695349dfd610cca
DESKTOP-ID70JJB\ChieuNhatNang-Office <chieunhatnang91@gmail.com>
2026-03-10 10:11:42 +0700

    Add mutex to ensure only 1 instance of the app running to prevent message messed up

--- RestMyAss/Program.cs
+++ RestMyAss/Program.cs
@@ -1,22 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace RestMyAss
 {
     static class Program
     {
+        private const string SingleInstanceMutexName = "RestMyAss_SingleInstance_Mutex";
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frm_Setting());
+            bool isFirstInstance;
+            using (Mutex mutex = new Mutex(true, SingleInstanceMutexName, out isFirstInstance))
+            {
+                if (!isFirstInstance)
+                {
+                    return;
+                }
+
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new frm_Setting());
+            }
         }
     }
 }
